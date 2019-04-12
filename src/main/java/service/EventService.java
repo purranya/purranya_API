329e9651bd6d7604aabb9e0de77373192322e080
malinowskikam <malinowskikam96@gmail.com>
2019-04-12 21:21:55 +0200

    Event service rebuild

--- src/main/java/service/EventService.java
+++ src/main/java/service/EventService.java
@@ -1,120 +1,147 @@
 package service;
 
 import app.DBInfo;
-import models.db_models.Calendar;
 import models.db_models.Event;
-import models.db_models.Label;
 import org.joda.time.DateTime;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class EventService {
     private Connection connection;
-    private ResultSet resultSet;
 
-    private static String CREATE_TABLE_SQL = "CREATE TABLE Event (\n" +
-            "    \"id\" bigserial PRIMARY KEY,\n" +
-            "    \"name\" varchar(50) CHECK(length(\"name\")>1 and length(\"name\")<51) NOT NULL,\n" +
-            "    \"comment\" varchar(255) CHECK(length(\"comment\")>0 and length(\"comment\")<256),\n" +
-            "    \"start_date\" timestamp NOT NULL,\n" +
-            "    \"end_date\" timestamp NOT NULL,\n" +
-            "    \"calendar_id\" bigint NOT NULL REFERENCES Calendar(\"id\"),\n" +
-            "    \"label_id\" bigint REFERENCES Label(\"id\")\n" +
-            ");";
-    private static String SELECT_SQL = "SELECT \"id\", \"name\", \"comment\", \"start_date\", \"end_date\", \"calendar_id\", \"label_id\" FROM Event " +
-            "WHERE \"name\" = ? AND \"comment\" = ? AND \"start_date\" = ? AND \"end_date\" = ? AND \"calendar_id\" = ? AND \"label_id\" = ? LIMIT 1";
-    private static String ADD_SQL = "INSERT INTO Event(\"id\", \"name\", \"comment\", \"start_date\", \"end_date\", \"calendar_id\", \"label_id\") VALUES (?, ?, ?, ?, ?, ?)";
-    private static String UPDATE_SQL = "UPDATE Event SET \"name\" = ? AND \"comment\" = ? AND \"start_date\" = ? AND \"end_date\" = ? AND \"calendar_id\" = ? AND \"label_id\" = ? WHERE \"id\" = ?";
-    private static String DELETE_SQL = "DELETE FROM Event WHERE \"id\" = ?";
+    private static String SQL_SELECT_BY_ID = "SELECT \"id\",\"name\",\"comment\",\"start_date\",\"end_date\",\"calendar_id\",\"label_id\" FROM Event WHERE \"id\"=? LIMIT 1";
+    private static String SQL_ADD = "INSERT INTO Event(\"id\", \"name\", \"comment\", \"start_date\", \"end_date\", \"calendar_id\", \"label_id\") VALUES (?, ?, ?, ?, ?, ?)";
+    private static String SQL_UPDATE = "UPDATE Event SET \"name\" = ? AND \"comment\" = ? AND \"start_date\" = ? AND \"end_date\" = ? AND \"calendar_id\" = ? AND \"label_id\" = ? WHERE \"id\" = ?";
+    private static String SQL_DELETE = "DELETE FROM Event WHERE \"id\" = ?";
+    private static String SQL_SELECT_BY_CALENDAR = "SELECT \"id\",\"name\",\"comment\",\"start_date\",\"end_date\",\"calendar_id\",\"label_id\" FROM Event WHERE \"calendar_id\"=?";
 
-    private PreparedStatement CREATE_TABLE_PSTM = null;
-    private PreparedStatement SELECT_PSTM = null;
+    private PreparedStatement SELECT_BY_ID_PSTM = null;
+    private PreparedStatement SELECT_BY_CALENDAR_PSTM = null;
     private PreparedStatement ADD_PSTM = null;
     private PreparedStatement UPDATE_PSTM = null;
     private PreparedStatement DELETE_PSTM = null;
 
     public EventService() throws SQLException {
         DBInfo dbInfo = new DBInfo();
-        boolean tableExists = false;
 
         connection = DriverManager.getConnection(dbInfo.get("jdbc_conn"), dbInfo.get("db_username"), dbInfo.get("db_password"));
-        resultSet = connection.getMetaData().getTables(null, null, null, null);
-        while(resultSet.next())
-            if("Event".equalsIgnoreCase(resultSet.getString("table_name"))) {
-                tableExists = true;
-                break;
-            }
-        if(!tableExists) {
-            CREATE_TABLE_PSTM = connection.prepareStatement(CREATE_TABLE_SQL);
-            CREATE_TABLE_PSTM.executeUpdate();
+        try
+        {
+            connection = DriverManager.getConnection(dbInfo.get("jdbc_conn"), dbInfo.get("db_username"), dbInfo.get("db_password"));
+        } catch (Exception e)
+        {
+            e.printStackTrace();
         }
     }
 
-    public boolean select(Calendar calendar, Label label, Event event) {
+    public Event getById(Long id){
         try {
-            SELECT_PSTM = connection.prepareStatement(SELECT_SQL);
+            if(SELECT_BY_ID_PSTM==null)
+                SELECT_BY_ID_PSTM = connection.prepareStatement(SQL_SELECT_BY_ID);
 
-            SELECT_PSTM.setString(1, event.getName());
-            SELECT_PSTM.setString(2, event.getComment());
-            SELECT_PSTM.setString(3, event.getStartDate().toString());
-            SELECT_PSTM.setString(4, event.getEndDate().toString());
-            SELECT_PSTM.setLong(5, calendar.getId());
-            SELECT_PSTM.setLong(6, label.getId());
-            SELECT_PSTM.executeUpdate();
-            return true;
+            SELECT_BY_ID_PSTM.setLong(1, id);
+            ResultSet rs = SELECT_BY_ID_PSTM.executeQuery();
+
+            if(rs.next())
+            {
+                Event e = new Event();
+                e.setId(rs.getLong(1));
+                e.setName(rs.getString(2));
+                e.setComment(rs.getString(3));
+                e.setStartDate(new DateTime(rs.getString(4)));
+                e.setEndDate(new DateTime(rs.getString(5)));
+                e.setCalendar_id(rs.getLong(6));
+                e.setLabel_id(rs.getLong(7));
+
+                return e;
+            }
+            else
+                return null;
         } catch(SQLException e) {
             e.printStackTrace();
-            return false;
+            return null;
         }
     }
 
-    public boolean add(Calendar calendar, Label label, Event event) {
+    public boolean add(Event event) {
         try {
-            ADD_PSTM = connection.prepareStatement(ADD_SQL);
+            if(ADD_PSTM==null)
+                ADD_PSTM = connection.prepareStatement(SQL_ADD);
 
             ADD_PSTM.setString(1, event.getName());
             ADD_PSTM.setString(2, event.getComment());
             ADD_PSTM.setString(3, event.getStartDate().toString());
             ADD_PSTM.setString(4, event.getEndDate().toString());
-            ADD_PSTM.setLong(5, calendar.getId());
-            ADD_PSTM.setLong(6, label.getId());
-            ADD_PSTM.executeUpdate();
-            return true;
+            ADD_PSTM.setLong(5, event.getCalendar_id());
+            ADD_PSTM.setLong(6, event.getLabel_id());
+            int added = ADD_PSTM.executeUpdate();
+            return added>0;
         } catch(SQLException e) {
             e.printStackTrace();
             return false;
         }
     }
 
-    public boolean update(Event event, Calendar calendar, Label label, String newName, String newComment, DateTime newStartDate, DateTime newEndDate, Long newLabel) {
+    public boolean update(Event event) {
         try {
-            UPDATE_PSTM = connection.prepareStatement(UPDATE_SQL);
+            if(UPDATE_PSTM==null)
+                UPDATE_PSTM = connection.prepareStatement(SQL_UPDATE);
 
-            UPDATE_PSTM.setString(1, newName);
-            UPDATE_PSTM.setString(2, newComment);
-            UPDATE_PSTM.setString(3, newStartDate.toString());
-            UPDATE_PSTM.setString(4, newEndDate.toString());
-            UPDATE_PSTM.setLong(5, calendar.getId());
-            UPDATE_PSTM.setLong(6, newLabel);
+            UPDATE_PSTM.setString(1, event.getName());
+            UPDATE_PSTM.setString(2, event.getComment());
+            UPDATE_PSTM.setString(3, event.getStartDate().toString());
+            UPDATE_PSTM.setString(4, event.getEndDate().toString());
+            UPDATE_PSTM.setLong(5, event.getCalendar_id());
+            UPDATE_PSTM.setLong(6, event.getLabel_id());
             UPDATE_PSTM.setLong(7, event.getId());
-            UPDATE_PSTM.executeUpdate();
-            return true;
+            int updated = UPDATE_PSTM.executeUpdate();
+            return updated>0;
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
         }
     }
 
-    public boolean delete(Event event) {
+    public boolean delete(Long id) {
         try {
-            DELETE_PSTM = connection.prepareStatement(DELETE_SQL);
+            if(DELETE_PSTM==null)
+                DELETE_PSTM = connection.prepareStatement(SQL_DELETE);
 
-            DELETE_PSTM.setLong(1, event.getId());
-            UPDATE_PSTM.executeUpdate();
-            return true;
+            DELETE_PSTM.setLong(1, id);
+            int deleted = UPDATE_PSTM.executeUpdate();
+            return deleted>0;
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
+        }
+    }
+
+    public ArrayList<Event> getByCalendar(Long id)
+    {
+        try {
+            if(SELECT_BY_CALENDAR_PSTM==null)
+                SELECT_BY_CALENDAR_PSTM = connection.prepareStatement(SQL_SELECT_BY_CALENDAR);
+
+            SELECT_BY_CALENDAR_PSTM.setLong(1, id);
+            ResultSet rs = SELECT_BY_ID_PSTM.executeQuery();
+            ArrayList<Event> events = new ArrayList<>();
+            while(rs.next())
+            {
+                Event e = new Event();
+                e.setId(rs.getLong(1));
+                e.setName(rs.getString(2));
+                e.setComment(rs.getString(3));
+                e.setStartDate(new DateTime(rs.getString(4)));
+                e.setEndDate(new DateTime(rs.getString(5)));
+                e.setCalendar_id(rs.getLong(6));
+                e.setLabel_id(rs.getLong(7));
+                events.add(e);
+            }
+            return events;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
