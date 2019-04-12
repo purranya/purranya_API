125d1560c0145218ea90859428adcbde6d77e2f1
malinowskikam <malinowskikam96@gmail.com>
2019-04-12 19:29:31 +0200

    CalendarService rebuild

--- src/main/java/service/CalendarService.java
+++ src/main/java/service/CalendarService.java
@@ -4,23 +4,24 @@ import app.DBInfo;
 import models.db_models.Calendar;
 import models.db_models.User;
 
+import javax.xml.ws.soap.Addressing;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class CalendarService {
     private Connection connection;
-    private ResultSet resultSet;
 
-    private final String SELECT_SQL = "SELECT \"id\", \"name\", \"comment\", \"appuser_id\" FROM Calendar " +
-            "WHERE \"name\" = ? AND \"comment\" = ? AND \"appuser_id\" = ?";
-    private final String ADD_SQL = "INSERT INTO Calendar(\"name\", \"comment\",\"appuser_id\") VALUES (?, ?, ?)";
-    private final String UPDATE_SQL = "UPDATE Calendar SET \"name\" = ?, \"comment\" = ? WHERE \"id\" = ?";
-    private final String DELETE_SQL = "DELETE FROM Calendar WHERE \"id\" = ?";
-
-    private PreparedStatement SELECT_PSTM = null;
-    private PreparedStatement CREATE_TABLE_PSTM = null;
+    private PreparedStatement SELECT_BY_ID_PSTM = null;
+    private PreparedStatement SELECT_BY_USER_PSTM = null;
     private PreparedStatement ADD_PSTM = null;
     private PreparedStatement UPDATE_PSTM = null;
     private PreparedStatement DELETE_PSTM = null;
+
+    private static String SQL_SELECT_BY_ID = "SELECT \"id\",\"name\",\"comment\",\"user_id\" FROM Calendar WHERE \"id\" = ? LIMIT 1";
+    private static String SQL_SELECT_BY_USER = "SELECT \"id\",\"name\",\"comment\",\"user_id\" FROM Calendar WHERE \"user_id\" = ?";
+    private static String SQL_ADD = "INSERT INTO Calendar(\"id\",\"name\",\"comment\",\"user_id\") VALUES(?,?,?,?)";
+    private static String SQL_UPDATE = "UPDATE Calendar SET \"name\"=?,\"comment\"=?,\"user_id\"=? WHERE \"id\"=?";
+    private static String SQL_DELETE = "DELETE FROM Calendar WHERE \"id\"=?";
 
     public CalendarService() {
         DBInfo db = new DBInfo();
@@ -34,64 +35,100 @@ public class CalendarService {
         }
     }
 
-    public boolean select(Calendar calendar, User user) {
+    public Calendar getById(Long id) {
         try {
-            if(SELECT_PSTM==null)
-                SELECT_PSTM = connection.prepareStatement(SELECT_SQL);
+            if(SELECT_BY_ID_PSTM==null)
+                SELECT_BY_ID_PSTM = connection.prepareStatement(SQL_SELECT_BY_ID);
 
-            SELECT_PSTM.setString(1, calendar.getName());
-            SELECT_PSTM.setString(2, calendar.getComment());
-            SELECT_PSTM.setLong(3, user.getId());
-            return true;
+            SELECT_BY_ID_PSTM.setLong(1, id);
+            ResultSet rs = SELECT_BY_ID_PSTM.executeQuery();
+
+            if(rs.next())
+            {
+                Calendar c = new Calendar();
+                c.setId(rs.getLong(1));
+                c.setName(rs.getString(2));
+                c.setComment(rs.getString(3));
+                c.setUser_id(rs.getLong(4));
+                return c;
+            }
+            else
+                return null;
         } catch (Exception e) {
             e.printStackTrace();
-            return false;
+            return null;
         }
     }
 
-    public boolean add(Calendar calendar, User user) {
+    public boolean add(Calendar calendar) {
         try {
             if(ADD_PSTM==null)
-                ADD_PSTM = connection.prepareStatement(ADD_SQL);
+                ADD_PSTM = connection.prepareStatement(SQL_ADD);
 
-            ADD_PSTM.setString(1, calendar.getName());
-            ADD_PSTM.setString(2, calendar.getComment());
-            ADD_PSTM.setLong(3, user.getId());
-            ADD_PSTM.executeUpdate();
-            return true;
+            ADD_PSTM.setLong(1,calendar.getId());
+            ADD_PSTM.setString(2, calendar.getName());
+            ADD_PSTM.setString(3, calendar.getComment());
+            ADD_PSTM.setLong(4, calendar.getUser_id());
+            int addedCount = ADD_PSTM.executeUpdate();
+            return addedCount>0;
         } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
     }
 
-    public boolean update(Calendar calendar, String newName, String newComment) {
+    public boolean update(Calendar calendar) {
         try {
             if(UPDATE_PSTM==null)
-                UPDATE_PSTM = connection.prepareStatement(UPDATE_SQL);
+                UPDATE_PSTM = connection.prepareStatement(SQL_UPDATE);
 
-            UPDATE_PSTM.setString(1, newName);
-            UPDATE_PSTM.setString(2, newComment);
-            UPDATE_PSTM.setLong(3, calendar.getId());
-            UPDATE_PSTM.executeUpdate();
-            return true;
+            UPDATE_PSTM.setString(1, calendar.getName());
+            UPDATE_PSTM.setString(2, calendar.getComment());
+            UPDATE_PSTM.setLong(3, calendar.getUser_id());
+            UPDATE_PSTM.setLong(4, calendar.getId());
+            int updatedCount = UPDATE_PSTM.executeUpdate();
+            return updatedCount>0;
         } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
     }
 
-    public boolean delete(Calendar calendar) {
+    public boolean delete(Long id) {
         try {
             if(DELETE_PSTM==null)
-            DELETE_PSTM = connection.prepareStatement(DELETE_SQL);
-
-            DELETE_PSTM.setLong(1, calendar.getId());
-            DELETE_PSTM.executeUpdate();
-            return true;
+            DELETE_PSTM = connection.prepareStatement(SQL_DELETE);
+            DELETE_PSTM.setLong(1, id);
+            int deletedCount= DELETE_PSTM.executeUpdate();
+            return deletedCount>0;
         } catch (Exception e) {
             e.printStackTrace();
             return false;
+        }
+    }
+
+    public ArrayList<Calendar> getByUserId(Long userId)
+    {
+        try {
+            if(SELECT_BY_USER_PSTM==null)
+                SELECT_BY_USER_PSTM = connection.prepareStatement(SQL_SELECT_BY_USER);
+
+            SELECT_BY_USER_PSTM.setLong(1, userId);
+            ResultSet rs = SELECT_BY_ID_PSTM.executeQuery();
+            ArrayList<Calendar> calendars = new ArrayList<>();
+            while(rs.next())
+            {
+                Calendar c = new Calendar();
+                c.setId(rs.getLong(1));
+                c.setName(rs.getString(2));
+                c.setComment(rs.getString(3));
+                c.setUser_id(rs.getLong(4));
+                calendars.add(c);
+            }
+            return calendars;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
