d59a7c9ac7d12e1f60a5f8c7a62b011eb6ba8b83
crin <37940924+crinuyi@users.noreply.github.com>
2019-05-09 14:04:47 +0200

    added addCalendar method to EventController

---
+++ src/main/java/controllers/EventController.java
@@ -0,0 +1,59 @@
+package controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import models.db_models.Calendar;
+import models.db_models.Event;
+import models.db_models.Label;
+import models.db_models.User;
+import models.transfer_models.Login;
+import models.transfer_models.OperationStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import services.CalendarService;
+import services.EventService;
+import services.LabelService;
+import services.UserService;
+import utils.PasswordUtils;
+
+@Component
+@RestController
+public class EventController {
+    private UserService userService;
+    private LabelService labelService;
+    private CalendarService calendarService;
+    private EventService eventService;
+
+    @RequestMapping(value = "/event/add", method = RequestMethod.POST)
+    public String addCalendar(@RequestParam("login") String login, @RequestParam("model") String model) throws Exception {
+        if (userService == null)
+            userService = new UserService();
+        if (calendarService == null)
+            calendarService = new CalendarService();
+        if (labelService == null)
+            labelService = new LabelService();
+        if (eventService == null)
+            eventService = new EventService();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Login loginOM = objectMapper.readValue(login, Login.class);
+        Event eventOM = objectMapper.readValue(model, Event.class);
+
+        User user = userService.getByLogin(
+                loginOM.getUsername(), PasswordUtils.sha256(loginOM.getPassword()));
+        Calendar calendar = calendarService.getById(eventOM.getCalendar_id());
+        Label label = labelService.getById(eventOM.getLabel_id());
+
+        if (user != null &&
+                !eventOM.getCalendar_id().equals(calendar.getId()) &&
+                !eventOM.getLabel_id().equals(label.getId())) {
+            if (eventService.add(eventOM))
+                return objectMapper.writeValueAsString(OperationStatus.OPERATION_FAILED);
+            else
+                return objectMapper.writeValueAsString(OperationStatus.OPERATION_FAILED);
+        } else
+            return objectMapper.writeValueAsString(OperationStatus.OPERATION_FAILED);
+    }
+}
